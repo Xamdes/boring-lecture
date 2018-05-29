ca9d3f382ad9a9029b4a06263983c2c697e2af01
Steven Colburn and William Kinzig <william@>
2018-05-29 14:08:34 -0700

    test using variables with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(function()
 {
-  $("p#paragraph1").click(function()
-  {
-    $("p").removeClass();
-    $("p").addClass("show-border");
-  });
-
   $("h1").click(function()
   {
     $("p").removeClass("show-border");
+  });
+
+  $("h2").click(function()
+  {
+    $("p").removeClass("show-border");
+  });
+
+
+
+  $("p#paragraph1").click(function()
+  {
+    var pOne = $("#paragraph1");
+    var currentClass = "show-border";
+    if(pOne.hasClass(currentClass))
+    {
+      pOne.removeClass(currentClass);
+    }
+    else
+    {
+      pOne.removeClass();
+      pOne.addClass(currentClass);
+    }
   });
 
 });
